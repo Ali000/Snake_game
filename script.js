06b059cd73907013d53f8cc8c0b18e94c5bc8ffa
Ali Albanna <ali.al-banna@outlook.com>
2024-01-18 10:51:29 +0300

    grid-area is tested to increase size of the snake

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const creatGridArray = () => {
   let arr = [];
   for(let i = 0; i < 20; i++) {
-    arr.push([]);
+    arr.push([i]);
   }
   return arr;
 }
 
 let gridArray = creatGridArray();
-console.log(gridArray);
+// console.log(gridArray);
+
