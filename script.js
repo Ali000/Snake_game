c2af073639819a3c25b299c0680275a0650f11a8
Ali Albanna <ali.al-banna@outlook.com>
2024-01-19 19:47:20 +0300

    snake moves as expected & food is genrating randomly, when food is eaten it will randomly reappear

--- script.js
+++ script.js
@@ -4,7 +4,10 @@ let snakeArray = [[1, 1]];
 let directionArray = [[1, 1]];
 let counter = 0; //counter to stop the while loop in case I miss up
 let lastKey = ""; // last key pressed on keyboard
+let foodx;
+let foody;
 const snake = document.querySelector("#snake");
+const food = document.querySelector(".food");
 
 const creatGridArray = () => {
   let arr = [];
@@ -19,43 +22,48 @@ const creatGridArray = () => {
 gridArray = creatGridArray();
 
 const generateFood = () => {
-  const food = document.querySelector(".food");
-  let x = Math.floor(Math.random() * 20)
-  let y = Math.floor(Math.random() * 20)
-  food.style.gridArea = `${x} / ${y}`;
+  foodx = Math.floor(Math.random() * 15) + 3;
+  foody = Math.floor(Math.random() * 15) + 3;
+  food.style.gridArea = `${foodx} / ${foody}`;
   food.style.display = "block";
-  gridArray[x][y] = "Food";
+  gridArray[foodx][foody] = "Food";
 };
-// generateFood();
+generateFood();
+
+const growSnake = () => {
+
+};
 document.addEventListener("keydown", (key) => {
   lastKey = key.code;
   move(lastKey);
 });
 
 const move = (key) => {
-  if((directionArray[0][0] > 20 || directionArray [0][1] > 20) || directionArray[0][0] < 0 || directionArray[0][1] < 0) {
+  if((directionArray[0][0] > 19 || directionArray [0][1] > 19) || directionArray[0][0] < 1 || directionArray[0][1] < 1) {
     gameStart = false;
   }
   if(gameStart === true && key === lastKey) {
     setTimeout(() => {
       if(key === "KeyD") {
         directionArray[0][1] += 1;
-        snake.style.gridArea = `${directionArray[0][0]} / ${directionArray[0][1]}`;
       } else if(key === "KeyA") {
         directionArray[0][1] -= 1;
-        snake.style.gridArea = `${directionArray[0][0]} / ${directionArray[0][1]}`;
-
       } else if(key === "KeyS") {
         directionArray[0][0] += 1;
-        snake.style.gridArea = `${directionArray[0][0]} / ${directionArray[0][1]}`;
-
       } else if(key === "KeyW") {
         directionArray[0][0] -= 1;
-        snake.style.gridArea = `${directionArray[0][0]} / ${directionArray[0][1]}`;
       }
-      console.log(directionArray)
+      snake.style.gridArea = `${directionArray[0][0]} / ${directionArray[0][1]}`;
+      if(directionArray[0][0] === foodx && directionArray[0][1] === foody) {
+        console.log("🚀 ~ setTimeout ~ directionArray[0][1]:", directionArray[0][1])
+        console.log("🚀 ~ setTimeout ~ directionArray:[0][0]", directionArray[0][0])
+        console.log("🚀 ~ setTimeout ~ foody:", foody)
+        console.log("🚀 ~ setTimeout ~ foodx:", foodx)
+        food.style.display = "none";
+        generateFood();
+      }
       move(key);
-  }, 200);
+  }, 150);
 }
 };
 //listen for key pressed
