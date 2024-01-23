aa67418be1df9d0aa4c3d3138046d8c149375ddd
Ali Albanna <ali.al-banna@outlook.com>
2024-01-23 14:05:14 +0300

    clean/tidy the code

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let gameStart = true;
-let gridArray;
 let snakeArray = [{x: 1, y: 1}];
 let directionArray = [[1, 1]];
-let turnArray = [];
-let counter = 0;
 let lastKey = "";
 let ateFood = false;
 let foodx;
@@ -67,24 +64,11 @@ const growSnake = (x, y) => {
 };
 growSnake(directionArray[0][0], directionArray[0][1]);
 
-const creatGridArray = () => {
-  let arr = [];
-  for(let i = 0; i < 20; i++) {
-    arr.push([]);
-    for(let j = 0; j < 20; j++) {
-      arr[i].push(0);
-    }
-  }
-  return arr;
-}
-gridArray = creatGridArray();
-
 const generateFood = () => {
   foodx = Math.floor(Math.random() * 15) + 3;
   foody = Math.floor(Math.random() * 15) + 3;
   food.style.gridArea = `${foodx} / ${foody}`;
   food.style.display = "block";
-  gridArray[foodx][foody] = "Food";
 };
 generateFood();
 
@@ -94,7 +78,6 @@ document.addEventListener("keydown", (key) => {
 });
 
 const playPause = () => {
-  console.log("pause/play");
   if(gameStart) {
     gameStart = false;
     play.style.display = "block";
